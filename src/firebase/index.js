f9370e29d820e2ef4959ded316c4ef7329a769c4
Amar Gupta <amar9911056196@gmail.com>
2020-08-21 15:11:09 +0530

    updating

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,17 +2,17 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/storage';
 import 'firebase/auth';
-const firebaseConfig = {
-  apiKey: 'AIzaSyCjlF_QEwMUqI2edFPacE4BUmGXJLSlPaQ',
-  authDomain: 'whatsapp-v2o.firebaseapp.com',
-  databaseURL: 'https://whatsapp-v2o.firebaseio.com',
-  projectId: 'whatsapp-v2o',
-  storageBucket: 'whatsapp-v2o.appspot.com',
-  messagingSenderId: '975150299639',
-  appId: '1:975150299639:web:d3d003c0019be120df16f2',
-  measurementId: 'G-B4G1PK78LL',
-};
-firebase.initializeApp(firebaseConfig);
+const Config = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
+firebase.initializeApp(Config);
 
 const firestore = firebase.firestore();
 export const auth = firebase.auth();
